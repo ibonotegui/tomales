b1e3a76e48d2f351eb17da3ca965a20127a59d4d
Ibon Otegui <ibon.otegui@gmail.com>
2025-02-14 11:12:24 -0800

    added a test

---
+++ app/src/androidTest/java/io/github/ibonotegui/tomales/MainScreenTest.kt
@@ -0,0 +1,31 @@
+package io.github.ibonotegui.tomales
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import io.github.ibonotegui.tomales.repository.LocalDatasource
+import io.github.ibonotegui.tomales.repository.Repository
+import io.github.ibonotegui.tomales.viewmodel.MainViewModel
+import org.junit.Rule
+import org.junit.Test
+
+class MainScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun mainScreen_dataDisplayed() {
+        val repository = Repository(LocalDatasource())
+        val mainViewModel = MainViewModel(repository)
+        mainViewModel.getSortedItems()
+
+        composeTestRule.setContent {
+            ItemsList(mainViewModel)
+        }
+
+        composeTestRule
+            .onNodeWithText("Item 4")
+            .assertIsDisplayed()
+    }
+}
