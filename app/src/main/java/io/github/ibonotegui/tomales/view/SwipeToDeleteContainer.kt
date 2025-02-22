37d4d789273fd7856611fcaf010acf6cc3070f67
Ibon Otegui <ibon.otegui@gmail.com>
2025-02-21 19:20:54 -0800

    added long press

--- app/src/main/java/io/github/ibonotegui/tomales/view/SwipeToDeleteContainer.kt
+++ app/src/main/java/io/github/ibonotegui/tomales/view/SwipeToDeleteContainer.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -28,6 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
@@ -85,16 +87,30 @@ fun <T> SwipeToDeleteContainer(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DeleteItemRow(swipeToDismissBoxState: SwipeToDismissBoxState, onDelete: () -> Unit) {
-    val color = when (swipeToDismissBoxState.dismissDirection) {
-        SwipeToDismissBoxValue.StartToEnd -> Color.Green
-        SwipeToDismissBoxValue.EndToStart -> Color.Red
-        SwipeToDismissBoxValue.Settled -> Color.Transparent
+    var longPressed by remember { mutableStateOf(false) }
+    val color = if (longPressed) {
+        Color.Red
+    } else {
+        when (swipeToDismissBoxState.dismissDirection) {
+            SwipeToDismissBoxValue.StartToEnd -> Color.Green
+            SwipeToDismissBoxValue.EndToStart -> Color.Red
+            SwipeToDismissBoxValue.Settled -> Color.Transparent
+        }
     }
     Row(
         modifier = Modifier
             .fillMaxSize()
             .background(color)
-            .padding(12.dp, 8.dp),
+            .padding(12.dp, 8.dp).pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        longPressed = false
+                    },
+                    onLongPress = {
+                        longPressed = true
+                    }
+                )
+            },
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.End
     ) {
